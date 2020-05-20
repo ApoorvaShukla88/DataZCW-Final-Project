bea1556ca40b6879cb171ae9740c88e43d16dec0
Apoorvashukla88 <apoorvashukla88@gmail.com>
2020-05-19 23:01:53 -0400

    graph_update

--- sentiment_graph_dash.py
+++ sentiment_graph_dash.py
@@ -10,18 +10,7 @@ app.layout = html.Div(children=[
     html.H1(children='Sentiments'),
     dcc.Graph()
 ])
-dcc.Graph(
-        id='example',
-        figure={
-            'data': [
-                {'x': [1, 2, 3, 4, 5], 'y': [9, 6, 2, 1, 5], 'type': 'line', 'name': 'Boats'},
-                {'x': [1, 2, 3, 4, 5], 'y': [8, 7, 2, 7, 3], 'type': 'bar', 'name': 'Cars'},
-            ],
-            'layout': {
-                'title': 'Basic Dash Example'
-            }
-        }
-    )
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
